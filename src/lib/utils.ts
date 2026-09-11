47a9465b2e4efa60044ed1a0edd8e9d354dd5605
William <atozniche1@gmail.com>
2026-09-11 22:35:04 +0200

    Update gift card purchase URL

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,7 +3,7 @@ import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) { return twMerge(clsx(inputs)); }
 export const API_URL = (import.meta.env.VITE_API_URL || '').replace(/\/$/, '');
-export const GIFT_CARD_LINK = 'https://www.g2a.com/rewarble-visa-gift-card-50-eur-by-rewarble-key-global-i10000502992084';
+export const GIFT_CARD_LINK = 'https://www.g2a.com/rewarble-visa-gift-card-50-eur-by-rewarble-key-global-i10000502992084?uuid=7f819364-c348-43ae-b186-5f592e8a422e';
 export const CONTACT_INFO = {
   email: 'candidfancom@gmail.com',
   reviewTime: 'within 12 hours',
